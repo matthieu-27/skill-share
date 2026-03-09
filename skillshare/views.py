c7d9b15f951636d6fdf8672c59c6a2635447702c
matthieu-27 <matthieu27@pm.me>
2026-03-09 14:02:29 +0100

    feat : created index page

--- skillshare/views.py
+++ skillshare/views.py
@@ -1,3 +1,6 @@
+from typing import Any
+
+from django.utils import timezone  # type: ignore
 from django.views.generic import ListView  # type: ignore
 
 from .models import Schedule
@@ -6,3 +9,8 @@ from .models import Schedule
 class HomePageView(ListView):
     model = Schedule
     template_name = "skillshare/index.html"
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context["now"] = timezone.now()
+        return context
