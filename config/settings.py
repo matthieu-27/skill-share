a791e2e9c73dc7f8efddf58561f4e23b90d953c6
matthieu-27 <matthieu27@pm.me>
2026-03-11 09:44:38 +0100

    feat: add get_full_name helper

--- config/settings.py
+++ config/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
-
+ACCOUNT_USER_DISPLAY = lambda user: user.get_full_name()  # noqa: E731
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
