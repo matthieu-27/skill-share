08e1295f67d7c1cacdcc6c0d450c7e58a2d0cd06
matthieu-27 <matthieu27@pm.me>
2026-03-13 15:20:40 +0100

    add Connection and Message many to many relationship

--- skillshare/models.py
+++ skillshare/models.py
@@ -1,7 +1,7 @@
+from typing import Any
+
 from django.conf import settings  # type: ignore
-from django.contrib.auth.models import (  # type: ignore
-    AbstractUser,
-)
+from django.contrib.auth.models import AbstractUser  # type: ignore
 from django.db import models  # type: ignore
 from django.utils import timesince, timezone  # type: ignore
 
@@ -63,7 +63,7 @@ class Schedule(models.Model):
     def __str__(self) -> str:
         return self.scheduled_at.isoformat()
 
-    def time_since(self):
+    def time_since(self) -> str:
         return timesince.timesince(self.scheduled_at, timezone.now())
 
 
@@ -77,6 +77,32 @@ class CustomUser(AbstractUser):
     slug = models.SlugField(unique=True, blank=True)
     skills = models.ManyToManyField(Skill, blank=True)
 
-    def save(self, *args, **kwargs):
+    def save(self, *args: Any, **kwargs: Any) -> None:
         self.slug = get_random_slug()
         super().save(*args, **kwargs)
+
+
+class Message(models.Model):
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    read_at = models.DateTimeField(null=True, blank=True)
+
+
+class Connection(models.Model):
+    """
+    A connection is created when a user seaking help on a skill already has a user with that skill
+    having a Schedule where is_request is None
+    """
+
+    taker = models.ForeignKey(
+        CustomUser, on_delete=models.CASCADE, related_name="connections_taken"
+    )
+    giver = models.ForeignKey(
+        CustomUser, on_delete=models.CASCADE, related_name="connections_given"
+    )
+    schedule = models.ForeignKey(
+        Schedule, on_delete=models.CASCADE, related_name="connections_schedule"
+    )
+    is_activated = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    chat = models.ManyToManyField(Message, related_name="connections_messages")
